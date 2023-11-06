a75b336e740ee98dea3d057d2ba73f3511589931
Florian <hi@florianpanchout.com>
2023-11-06 08:40:52 +0100

    feat: add some params and a new route

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const withSplit = require('next-with-split').withSplit({
   splits: {
     example1: { // Identification of A/B tests (any)
-      path: '/*', // Paths to perform A/B testing. (regular expression)
+      path: '/test', // Paths to perform A/B testing. (regular expression)
       hosts: {
         // [branch name]: host name
-        original: 'test-split.vercel.app',
-        'challenger': 'test-split-git-challenger-nos-gestes-climat.vercel.app',
+        main: 'test-split.vercel.app',
+        'challenger': { host: 'test-split-git-challenger-nos-gestes-climat.vercel.app', weight: 10 },
       },
       cookie: { // Optional (For Sticky's control)
         maxAge: 60 * 60 * 12 * 1000 // Number of valid milliseconds for sticky sessions. (default is 1 day)
       }
     },
-  }
+  },
+  isOriginal: true,
+  hostname: 'test-split.vercel.app',
+  currentBranch: 'main'
 })
 
 module.export = withSplit({
