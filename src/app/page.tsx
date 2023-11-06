43b6b4c01a14b79418631af435bca68fa556377e
Florian <hi@florianpanchout.com>
2023-11-06 08:17:57 +0100

    feat: introduce change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>MAIN</h1>       
+      <h1>CHALLENGER</h1>       
     </main>
   )
 }
