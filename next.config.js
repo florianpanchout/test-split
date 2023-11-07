0a44a2256b6276a6844941321d72e05808030d4f
Florian <hi@florianpanchout.com>
2023-11-07 07:08:43 +0100

    feat: use homemade redirect

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-const withSplit = require('next-with-split').withSplit({
-  splits: {
-    example1: { // Identification of A/B tests (any)
-      path: '/test', // Paths to perform A/B testing. (regular expression)
-      hosts: {
-        // [branch name]: host name
-        main: 'test-split.vercel.app',
-        'challenger': { host: 'test-split-git-challenger-nos-gestes-climat.vercel.app', weight: 10 },
-      },
-      cookie: { // Optional (For Sticky's control)
-        maxAge: 60 * 60 * 12 * 1000 // Number of valid milliseconds for sticky sessions. (default is 1 day)
-      }
-    },
-  },
-  isOriginal: false,
-  hostname: 'test-split-git-challenger-nos-gestes-climat.vercel.app',
-  currentBranch: 'challenger'
-})
-
-module.export = withSplit({
+module.export = {
   reactStrictMode: true,
-})
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,12 @@
-// middleware.ts
-export { middleware } from 'next-with-split'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import type { NextMiddlewareResult } from 'next/dist/server/web/types'
+
+export const middleware = (req: NextRequest): NextMiddlewareResult => {
+  const rewriteTo = `https://test-split.vercel.app/`
+  const isExternal = rewriteTo.startsWith('http')
+
+  if (isExternal) return NextResponse.rewrite(rewriteTo)
+
+  return NextResponse.next()
+}
